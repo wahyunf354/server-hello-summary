18f87de16f2060cf30e5dc665b4976ec98254515
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-05-02 19:33:12 +0700

    read data social media

--- Controller/SocialMediaController.js
+++ Controller/SocialMediaController.js
@@ -1,11 +1,38 @@
-function index(req, res) {
-  res.render("admin/social_media", { title: "Hello Summer | Sosial Media" });
-}
+const SocialMedia = require("../Models/SocialMedia");
 
-function addData(req, res) {
+const index = async (req, res) => {
+  try {
+    const data = await SocialMedia.find();
+    // console.log(data);
+    res.render("admin/social_media", {
+      title: "Hello Summer | Sosial Media",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const addData = async (req, res) => {
   const { username, media } = req.body;
-  // console.log(username, media);
-}
+  console.log(username, media);
+  try {
+    await SocialMedia.create({ username, media });
+
+    res.redirect("/admin/social_media");
+  } catch (error) {
+    console.log("ini error", error);
+  }
+};
+
+const getAllData = async (req, res) => {
+  try {
+    const data = await SocialMedia.find();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   index,
