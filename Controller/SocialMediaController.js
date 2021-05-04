6392ef24165dea5e375288a9d7a66a59923824bb
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-05-04 18:28:56 +0700

    menambahkan alert di sosial media

--- Controller/SocialMediaController.js
+++ Controller/SocialMediaController.js
@@ -3,10 +3,11 @@ const SocialMedia = require("../Models/SocialMedia");
 const index = async (req, res) => {
   try {
     const data = await SocialMedia.find();
-    // console.log(data);
     res.render("admin/social_media", {
       title: "Hello Summer | Sosial Media",
       data: data,
+      messages: req.flash("message"),
+      messageStatus: req.flash("messageStatus"),
     });
   } catch (error) {
     console.log(error);
@@ -18,10 +19,14 @@ const addData = async (req, res) => {
   console.log(username, media);
   try {
     await SocialMedia.create({ username, media });
-
+    req.flash("message", "Berhasil Menambah Data");
+    req.flash("messageStatus", "success");
     res.redirect("/admin/social_media");
   } catch (error) {
+    req.flash("message", "Gagal Menambah Data");
+    req.flash("messageStatus", "error");
     console.log("ini error", error);
+    res.redirect("/admin/social_media");
   }
 };
 
@@ -32,9 +37,14 @@ const editData = async (req, res) => {
     socialMedia.username = username;
     socialMedia.media = media;
     await socialMedia.save();
+    req.flash("message", "Berhasil Menambah Data");
+    req.flash("messageStatus", "success");
     res.redirect("/admin/social_media");
   } catch (error) {
+    req.flash("message", "Gagal Mengubah Data");
+    req.flash("messageStatus", "error");
     console.log(error);
+    res.redirect("/admin/social_media");
   }
 };
 
@@ -43,9 +53,15 @@ const deleteData = async (req, res) => {
     const { id } = req.params;
     const socialMedia = await SocialMedia.findOne({ _id: id });
     await socialMedia.remove();
+
+    req.flash("message", "Behasil Menghapus Data");
+    req.flash("messageStatus", "warning");
     res.redirect("/admin/social_media");
   } catch (error) {
+    req.flash("message", "Gagal Menhapus Data");
+    req.flash("messageStatus", "error");
     console.log(eror);
+    res.redirect("/admin/social_media");
   }
 };
 
--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const methodOverride = require("method-override");
 const favicon = require("serve-favicon");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -26,6 +28,15 @@ app.set("view engine", "ejs");
 
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
